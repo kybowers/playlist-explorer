da546506e467d9698350c0cbd6d5d6f9ab2d6a28
kybowers <kybowers@users.noreply.github.com>
2020-02-22 19:06:04 -0500

    add load more for playlists display

--- README.md
+++ README.md
@@ -5,17 +5,3 @@ Development will be sporadic.
 To run development: `yarn electron-dev`
 
 To build portable app: `yarn electron-pack`
-
-Planned features and known defects:
-
-- Need to validate API response contains all data before rendering to prevent crashes
-- Fetch more than the default 20 playlists
-- Fetch more than the default 100 tracks
-- Include data from track api in csv export
-- Users might want to refresh
-- Auth token need to be stored somewhere secure and semi-persistant
-- CSV generation may not guarantee order
-- Home/Login page should be more presentable
-- Smooth animations for selecting playlist
-- Data select tool for exporting data
-- Create app bar and remove frame
--- src/Components/MyPlaylists/index.jsx
+++ src/Components/MyPlaylists/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
 import PlaylistCard from "../PlaylistCard";
@@ -6,29 +7,37 @@ import apiService from "../../Services/apiService";
 
 const MyPlaylists = props => {
   const { token } = props;
-  const [playlists, setPlaylists] = useState(null);
+  const [playlists, setPlaylists] = useState([]);
+  const [total, setTotal] = useState(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
     const handleGetData = async () => {
-      const response = await apiService.get(token, "https://api.spotify.com/v1/me/playlists");
+      const response = await apiService.get(
+        token,
+        `https://api.spotify.com/v1/me/playlists?offset=${scrollPosition}&limit=1`
+      );
       const json = await response.json();
-      setPlaylists(json);
+      setTotal(json.total);
+      setPlaylists(playlists => [...playlists, ...json.items]);
     };
     handleGetData();
-  }, [token]);
+  }, [token, scrollPosition]);
 
   return (
-    <Grid container spacing={2}>
-      {playlists && playlists.items ? (
-        playlists.items.map(item => (
-          <Grid item xs={3} key={item.id}>
-            <PlaylistCard playlist={item} token={token}/>
+    <>
+      <Grid container spacing={2}>
+        {playlists.map(item => (
+          <Grid item xs={12} sm={6} md={3} key={item.id}>
+            <PlaylistCard playlist={item} token={token} />
           </Grid>
-        ))
-      ) : (
-        <Grid item>No Data</Grid>
+        ))}
+      </Grid>
+      {total && total > playlists.length && (
+        // TODO trigger load more on scroll, no button necessary
+        <Button onClick={() => setScrollPosition(scrollPosition => scrollPosition + 1)}>Load More</Button>
       )}
-    </Grid>
+    </>
   );
 };
 
